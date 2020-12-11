aefe3137666e971a5024de70359afe114e308aab
Bernardo Sequeira <bernardosequeir@gmail.com>
2020-12-11 19:29:53 +0000

    10.8 "done", just missing the styling

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,9 +1,33 @@
+import { Formik } from 'formik';
 import React from 'react';
-
+import { TouchableWithoutFeedback, StyleSheet, View } from 'react-native';
+import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 
+const styles = StyleSheet.create({
+  form: {
+    display: 'flex'
+  }
+});
+const initialValues = { username: '', password: '' };
+
+
+const onSubmit = (values) => {
+  console.log(values);
+};
+
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit} style={styles.form}>
+      <View style={styles.form}>
+        <FormikTextInput name="username" placeholder="Username" />
+        <FormikTextInput name="password" placeholder="Password" secureTextEntry />
+        <TouchableWithoutFeedback onPress={onSubmit}>
+          <Text>Sign In</Text>
+        </TouchableWithoutFeedback>
+      </View>
+    </Formik>
+  );
 };
 
 export default SignIn;
