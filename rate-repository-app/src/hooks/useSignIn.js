4cff7948300d0a8d5b15b621e064858a80d72f7e
Bernardo Sequeira <bernardosequeir@gmail.com>
2020-12-15 19:50:46 +0000

    completed the code until the last exercise block

--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,13 +1,19 @@
+import { useContext } from 'react';
 import { useMutation } from "@apollo/react-hooks";
 import { SIGN_IN } from '../graphql/mutations';
+import AuthStorageContext from '../contexts/AuthStorageContext';
 
 export const useSignIn = () => {
+  const authStorage = useContext(AuthStorageContext);
   const [mutate, result] = useMutation(SIGN_IN);
 
   const signIn = async ({ username, password }) => {
     const { data } = await mutate({
       variables: { credentials: { username, password } }
     });
+
+    await authStorage.setAccessToken(data.authorize.accessToken);
+
     return data;
   };
 
